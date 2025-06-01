404e8b9c75f8d481b9ea5853c7045b5680cfd6e6
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-06-01 21:05:09 +0300

    fix: add LoginResponse to exports

--- lib/telegram_login_flutter.dart
+++ lib/telegram_login_flutter.dart
@@ -1,6 +1,7 @@
 library telegram_login_flutter;
 
 export 'src/models/telegram_user.dart';
+export 'src/models/login_response.dart';
 export 'src/telegram_login.dart';
 export 'src/session.dart';
 export 'widgets/telegram_login_button.dart';
